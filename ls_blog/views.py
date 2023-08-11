2f0f5d64a4cc918514943062e8d2ea99c2558e6a
Igor Nikolovski <nikolovski_igor@icloud.com>
2023-08-11 22:19:45 +0200

    v11.08.23

--- ls_blog/views.py
+++ ls_blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def BaseIndex(request):
+  
+    
+    context = {
+    
+    }
+  
+    return render(request, 'blog/index.html', context)
