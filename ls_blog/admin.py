dddb01a6a8c70c65f6b421f1b5e38ebc096f4520
Igor Nikolovski <nikolovski_igor@icloud.com>
2023-09-01 21:42:21 +0200

    Profile

--- ls_blog/admin.py
+++ ls_blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Post, PostCategory
+from .models import Post, PostCategory, Profile
 
 admin.site.register(Post)
 admin.site.register(PostCategory)
+admin.site.register(Profile)
 
 # Register your models here.
--- ls_blog/models.py
+++ ls_blog/models.py
@@ -39,3 +39,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('blog:article-detail', args=(str(self.id)))
         return reverse('blog:index')
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user) 
